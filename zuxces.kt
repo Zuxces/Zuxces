3d68c66e9b5c641a54ea07ade6c58ae8a3221529
Zuxces <67604119+Zuxces@users.noreply.github.com>
2021-07-21 18:50:17 +0100

    Update zuxces.kt

--- zuxces.kt
+++ zuxces.kt
@@ -1,1 +1,12 @@
+import java.lang.StringBuilder
 
+fun main() {
+    val Zux = User(name = "Zuxces", age = 15, occupation = "Developer", nerd = true)
+    val user = StringBuilder()
+    user.append("Name: ${Zux.name}\n")
+    user.append("Age: ${Zux.age}\n")
+    user.append("Occupation: ${Zux.occupation}\n")                                                
+    user.append("Nerd: ${Zux.nerd}")
+    println(user)
+}
+data class User(val name: String, val age: Int, val occupation: String, val nerd: Boolean)
